9dcfa75f169e5278b16a781c80fda9f4966436b5
James@solarturtle.co.za-at-533493394428 <MyruUQOuFcEVRrVPRxVLlSek+VL2B5Gn/hr+jxQdJCo=>
2023-01-19 14:40:03 +0200

    Payment now working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ReactJson from 'react-json-pretty';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import Navigation from './navigation';
 import TransferFunds from './transferFunds';
 import Home from './home';
@@ -16,39 +16,36 @@ interface IProps {
     // your props here
 }
 
-let loading = false;
+//Check whether the app is loading or not
+let isLoading = false;
 
-const App: React.FC<IProps> = ({ }) => {
+const App: React.FC<IProps> = () => {
     const [clientToken, setClientToken] = useState<IData>({});
-    //const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState<Error | null>(null);
+
     useEffect(() => {
         //Check if not already loading to stop repeat API calls
-        if (!loading) {
-            loading = true;
+        if (!isLoading) {
+            isLoading = true;
             console.log("clientToken = " + JSON.stringify(clientToken));
-            //console.log("isLoading = " + isLoading);
-            console.log("Inside useEffect");
-            //setIsLoading(true);
             let timeoutId: NodeJS.Timeout;
             const fetchData = async () => {
                 console.log("Inside fetchData");
                 try {
                     timeoutId = setTimeout(() => {
-                        setError(new Error("Timeout Error"));
-                        loading = false;
+                        isLoading = false;
                         console.log("Timeout while waiting for client token")
                     }, 5000);
+                    //Get the client token for the session
                     const res = await retrieveTokenUsingClientSecret(clientInfo.client.id, clientInfo.secret.value);
-                    const tokenReceived = res.access_token;//JSON.stringify(res);//await res.json();
+                    const tokenReceived = res.access_token;
                     console.log("tokenReceived = ", tokenReceived);
                     setClientToken(tokenReceived as IData);
+
                     console.log("Set loading to false");
-                    loading = false;
+                    isLoading = false;
                 } catch (err) {
                     console.log("Error loading page",err);
-                    setError(err as Error);
-                    loading = false;
+                    isLoading = false;
                     clearTimeout(timeoutId);
                 }
             }
@@ -58,47 +55,61 @@ const App: React.FC<IProps> = ({ }) => {
                 console.log('fetch client token complete. Reset timeout')
             });
         }
-/*        return () => {
-            clearTimeout(timeoutId);
-        };*/
     
-    }, []); // empty array means the effect will only run once on mount
+    },[]); // empty array means the effect will only run once on mount
+
+    // create an instance of the ApolloClient with the client token received
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        link: new HttpLink({
+            uri: 'https://api.stitch.money/graphql',
+            headers: {
+                authorization: `Bearer ${clientToken}`
+            }
+        }),
+    });
+
     //Return the loading indicator if the ASYNC token call is still busy
-    if (loading) {
+    if (isLoading) {
         return (
             <div>
-                {clientToken ? <ReactJson data={clientToken}/> : <div>Loading...</div>}
+                <div>Loading...</div>
             </div>
         );
-    } else {
+    }
+    //If not loading then render the application with all it's components
+    else { 
         return (
-            <Router>
-                <table style={{ width: "90%" }} >
-                    <tbody>
-                        <tr>
-                            <td style={{ width: "200px" }}>
-                                <img src={logo} alt="Logo" style={{ width: "170px", height: "170px", padding: "20px" }} />
-                            </td>
-                            <td>
-                                <Navigation />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div id='side-panel'></div>
-                            </td>
-                            <td>
-                                <div id='main-body'>
-                                    <Routes>
-                                        <Route path="/" element={<Home />} />
-                                        <Route path="/transfer-funds" element={<TransferFunds />} />
-                                    </Routes>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </Router>
+            <ApolloProvider client={client}>
+
+                <Router>
+                    <table style={{ width: "90%" }} >
+                        <tbody>
+                            <tr>
+                                <td style={{ width: "200px" }}>
+                                    <img src={logo} alt="Logo" style={{ width: "170px", height: "170px", padding: "20px" }} />
+                                </td>
+                                <td>
+                                    <Navigation />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div id='side-panel'></div>
+                                </td>
+                                <td>
+                                    <div id='main-body'>
+                                        <Routes>
+                                            <Route path="/" element={<Home />} />
+                                            <Route path="/transfer-funds" element={<TransferFunds />} />
+                                        </Routes>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Router>
+            </ApolloProvider>
         );
     }
 }
--- src/stitchServices.tsx
+++ src/stitchServices.tsx
@@ -1,4 +1,64 @@
-﻿
+﻿import { gql } from '@apollo/client';
+
+export enum BankBeneficiaryBankId {
+    absa = "absa",
+    capitec = "capitec",
+    discovery_bank = "discovery_bank",
+    fnb = "fnb",
+    investec = "investec",
+    nedbank = "nedbank",
+    sasfin_bank = "sasfin_bank",
+    standard_bank = "standard_bank",
+    tymebank = "tymebank",
+    za_access_bank = "za_access_bank",
+    access_bank = "access_bank",
+    first_bank_of_nigeria = "first_bank_of_nigeria",
+    gtbank = "gtbank",
+    providus_bank = "providus_bank",
+    sterling_bank = "sterling_bank",
+    united_bank_for_africa = "united_bank_for_africa",
+    vfd_microfinance_bank = "vfd_microfinance_bank",
+    wema_bank = "wema_bank",
+    zenith_bank = "zenith_bank",
+    za_bidvest = "za_bidvest",
+    za_citibank = "za_citibank",
+    za_u_bank = "za_u_bank",
+    za_jp_morgan_chase_bank = "za_jp_morgan_chase_bank",
+    za_mercantile_bank = "za_mercantile_bank",
+    za_postbank = "za_postbank",
+    za_bank_windhoek = "za_bank_windhoek",
+    za_nedbank_namibia = "za_nedbank_namibia",
+    za_hbz_bank = "za_hbz_bank",
+    za_olympus_mobile = "za_olympus_mobile",
+    za_hsbc = "za_hsbc",
+    za_vbs_mutual_bank = "za_vbs_mutual_bank",
+    za_finbond_mutual_bank = "za_finbond_mutual_bank",
+    za_finbond_net1 = "za_finbond_net1",
+    za_bnp_paribas = "za_bnp_paribas",
+    za_habib_overseas_bank = "za_habib_overseas_bank",
+    za_people_bank = "za_people_bank",
+    za_standard_chartered_bank = "za_standard_chartered_bank",
+    za_ithala_bank = "za_ithala_bank",
+    za_unibank = "za_unibank",
+    za_albaraka_bank = "za_albaraka_bank",
+    za_state_bank_of_india = "za_state_bank_of_india",
+    za_bank_zero = "za_bank_zero",
+}
+
+export interface ITransaction {
+    amount: {
+        quantity: number;
+        currency: string;
+    };
+    payerReference: string;
+    beneficiaryReference: string;
+    externalReference: string;
+    beneficiaryName: string;
+    beneficiaryBankId: string;
+    beneficiaryAccountNumber: string;
+}
+
+
 /**
  * Retrieve the client access token
  **/
@@ -25,3 +85,37 @@ export async function retrieveTokenUsingClientSecret(clientId: string, clientSec
     console.log("Tokens: ", responseBody);
     return responseBody;
 }
+
+/*
+ * Define a graphQL mutation to create a payment request
+ */ 
+export const CREATE_PAYMENT_REQUEST = gql`
+  mutation CreatePaymentRequest(
+    $amount: MoneyInput!,
+    $payerReference: String!,
+    $beneficiaryReference: String!,
+    $externalReference: String,
+    $beneficiaryName: String!,
+    $beneficiaryBankId: BankBeneficiaryBankId!,
+    $beneficiaryAccountNumber: String!
+  ) {
+    clientPaymentInitiationRequestCreate(input: {
+      amount: $amount,
+      payerReference: $payerReference,
+      beneficiaryReference: $beneficiaryReference,
+      externalReference: $externalReference,
+      beneficiary: {
+        bankAccount: {
+          name: $beneficiaryName,
+          bankId: $beneficiaryBankId,
+          accountNumber: $beneficiaryAccountNumber
+        }
+      }
+    }) {
+      paymentInitiationRequest {
+        id
+        url
+      }
+    }
+  }
+`;
