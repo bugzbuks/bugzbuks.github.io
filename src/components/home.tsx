b3689619446310d3cb77bc6b1d9a9f951b271450
James@solarturtle.co.za-at-533493394428 <MyruUQOuFcEVRrVPRxVLlSek+VL2B5Gn/hr+jxQdJCo=>
2023-01-20 12:21:07 +0200

    Removed the HREK and used Link instead

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,14 @@
 ﻿import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div className="home-title">
             <h1>Thank you suport supporting your local charity</h1>
             <p>This utility allows you to make a donation you favourite NGO</p>
-            <p>Select <a className="link-info" href="/transfer-funds">"Transfer Funds"</a> then complete the form</p>
+            <p>Select <Link className="link-info" to="/transfer-funds">"Transfer Funds"</Link> then complete the form</p>
             <p>This is a demo project created by James van der Walt for Stitch</p>
         </div>
     );
