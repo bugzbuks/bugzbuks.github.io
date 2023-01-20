c78e27d6af8af83705c3524d22252451aa8aacaf
James@solarturtle.co.za-at-533493394428 <MyruUQOuFcEVRrVPRxVLlSek+VL2B5Gn/hr+jxQdJCo=>
2023-01-20 13:04:50 +0200

    Small alignment changes

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ Bootstrap is used for the UI formatting
 All fields are validated for input so that the API call to process the payment completes without fail
 A client token is received using the Client Id and Secret Value
 When the App start an Apollo client is configured to point to the Stich payment services, using the Bearer token received int he previous step. 
+GraphQL was used to make the mutaion call for the payment
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className="home-title">
             <h1>Thank you suport supporting your local charity</h1>
-            <p>This utility allows you to make a donation you favourite NGO</p>
+            <p>This utility allows you to make a donation your favourite NGO</p>
             <p>Select <Link className="link-info" to="/transfer-funds">"Transfer Funds"</Link> then complete the form</p>
             <p>This is a demo project created by James van der Walt for Stitch</p>
         </div>
